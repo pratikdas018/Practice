780bf69db000a71be338fd271675a5787219f6f5
pratikdas018 <pratikdassonu@gmail.com>
2024-09-10 11:50:18 +0530

    added github data

--- index.js
+++ index.js
@@ -3,6 +3,47 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+
+const githubData = 
+    {
+        "login": "pratikdas018",
+        "id": 178085148,
+        "node_id": "U_kgDOCp1dHA",
+        "avatar_url": "https://avatars.githubusercontent.com/u/178085148?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/pratikdas018",
+        "html_url": "https://github.com/pratikdas018",
+        "followers_url": "https://api.github.com/users/pratikdas018/followers",
+        "following_url": "https://api.github.com/users/pratikdas018/following{/other_user}",
+        "gists_url": "https://api.github.com/users/pratikdas018/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/pratikdas018/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/pratikdas018/subscriptions",
+        "organizations_url": "https://api.github.com/users/pratikdas018/orgs",
+        "repos_url": "https://api.github.com/users/pratikdas018/repos",
+        "events_url": "https://api.github.com/users/pratikdas018/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/pratikdas018/received_events",
+        "type": "User",
+        "site_admin": false,
+        "name": null,
+        "company": null,
+        "blog": "",
+        "location": null,
+        "email": null,
+        "hireable": null,
+        "bio": null,
+        "twitter_username": null,
+        "public_repos": 4,
+        "public_gists": 0,
+        "followers": 0,
+        "following": 1,
+        "created_at": "2024-08-11T12:42:26Z",
+        "updated_at": "2024-09-03T06:49:06Z"
+      }
+
+app.get("/github",(req,res)=>{
+    res.json(githubData)
+})
+
 app.get("/",(req,res)=>{
     res.send("hello");
 });
